0ef41c3c06df3b4d1cf23079228aee865c16d905
Cuiyc <yongchang.cui@icloud.com>
2025-12-17 09:20:27 -0500

    neeFix

---
+++ main.py
@@ -0,0 +1,49 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+#from langchain_openai import ChatOpenAI
+
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+@tool
+def search(query: str ) -> str:
+    """
+    Make internet search for the query
+    
+    :param query: anything to search from internet
+    :type query: str
+    :return: query result
+    :rtype: str
+    """
+    print(f"Searching for {query}")
+    return "Tokyo weather is sunny"
+
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash-lite",
+    temperature=1.0,  # Gemini 3.0+ defaults to 1.0
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
+
+def main():
+    print("Hello from langchain-course!")
+    result = agent.invoke(messages=[HumanMessage("How about weather of Tokyo")] )
+    print(result)
+
+
+
+
+if __name__ == "__main__":
+    main()
