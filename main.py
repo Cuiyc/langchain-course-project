4a571d40bb7bb56d6cc04dbbe12a2195be2419da
Cuiyc <yongchang.cui@icloud.com>
2025-12-17 08:34:49 -0500

    update

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import os
+
 from dotenv import load_dotenv
 
 from langchain_core.prompts import PromptTemplate
 
-from langchain_google_genai import ChatGoogleGenerativeAI
-
+from langchain_ollama import ChatOllama
 load_dotenv()
 
 #model = ChatOpenAI(model="gpt-4.1")
-model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
+#model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
+model = ChatOllama(temperature=0,model="qwen3:4b")
 def main():
     print("Hello from langchain-course!")
     information = """Elon Reeve Musk[b] (born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, X, and xAI. Musk has been the wealthiest person in the world since 2021; as of December 2025, Forbes estimates his net worth to be around $677 billion.[1]
